fecc52680e6765b95d67b17dd27c9bd11183067e
erbe1 <erik.bergstrom86@gmail.com>
2019-01-24 13:36:51 +0100

    Massa länkar

--- Tutor/Tutor/wwwroot/scripts/main.js
+++ Tutor/Tutor/wwwroot/scripts/main.js
@@ -24,13 +24,11 @@ let links = [
     Link("https://www.youtube.com/watch?v=gwD9awr3NNo", "Linq video tutorial", ["Backend"], ["C#"], ["Linq", "Video"], "Tutorial for learning queary-expressions with Linq", true),
     Link("https://www.codecademy.com/learn/learn-git", "Tutorial  GIT", ["Version control"], ["Git"], ["Git"], "Tutorial for the version control system GIT"),
     Link("https://www.youtube.com/watch?v=0pUVC3trvHc", "What's New in Entity Framework Core 2.0", ["DB"], ["EF Core 2.0"], ["EF", "DB", "EF Core", "Entity Framework", "Database"], "Video presentation from the microsoft-team on whats new in EF Core 2.0 and what the team has planned for upcoming releases.", true),
-Link("https://www.learncs.org/", "Learn C#", ["Backend"], ["C#"], ["C#", "Backend", "Interactive"], "Free interactive C# tutorial"),
+    Link("https://www.learncs.org/", "Learn C#", ["Backend"], ["C#"], ["C#", "Backend", "Interactive"], "Free interactive C# tutorial"),
     Link("https://csharpskolan.se/", "C# skolan", ["Backend"], ["C#"], ["C#", "Svenska", "Backend"], "Utbildningsmaterial i form av instruktionsvideor och artiklar på svenska"),
-    Link("https://www.learnjavaonline.org/"), "Learn Java", ["Backend"], ["Java"], ["Java", "Backend", "Interactive"], "Free interactive Java tutorial"),
-    Link("https://www.codecademy.com/learn/learn-java"), "Codeacademy", ["Backend"], ["Java"], ["Java", "Backend", "Interactive"], "Fundamental programming concepts, including object-oriented programming (OOP) using Java"),
-    Link("https://www.techbeamers.com/python-tutorial-step-by-step/"), "TechBeamers", ["Backend"], ["Python"], ["Python", "Backend", "Free"], "This Python tutorial is a one-stop programming guide for all beginners"),
-
-
+    Link("https://www.learnjavaonline.org/", "Learn Java", ["Backend"], ["Java"], ["Java", "Backend", "Interactive"], "Free interactive Java tutorial"),
+    Link("https://www.codecademy.com/learn/learn-java", "Codeacademy", ["Backend"], ["Java"], ["Java", "Backend", "Interactive"], "Fundamental programming concepts, including object-oriented programming (OOP) using Java"),
+    Link("https://www.techbeamers.com/python-tutorial-step-by-step/", "TechBeamers", ["Backend"], ["Python"], ["Python", "Backend", "Free"], "This Python tutorial is a one-stop programming guide for all beginners"),
 ];
 
 
