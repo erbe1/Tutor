58a28d09ae4763f6792d871003c41a71682aa995
Magnus Tidqvist <magnus.tidqvist@outlook.com>
2019-01-24 11:49:00 +0100

    lagt in javascript

--- Tutor/Tutor/wwwroot/scripts/main.js
+++ Tutor/Tutor/wwwroot/scripts/main.js
@@ -5,3 +5,30 @@
     });
 
 });
+
+
+function Link(linkadress, title, category, subCategory, embedded = false) {
+    Linkadress = linkadress;
+    Titel = title;
+    Category = category;
+    SubCategory = subCategory;
+    Embedded = embedded;
+}
+
+let links = [
+    new {
+        Linkadress: "https://fontawesome.com/",
+        Title = "Font Awesome",
+        Category =["Frontend"],
+        SubCategory =["Fonts"],
+        Embedded = false
+    },
+
+];
+
+let links = [
+    Link("https://fontawesome.com/", "Font Awesome", ["Frontend"], ["Fonts"]),
+    Link("https://fontawesome.com/", "Font Awesome", ["Frontend"], ["Fonts"]),
+];
+
+
