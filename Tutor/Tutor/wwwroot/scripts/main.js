acaf8100dd58b37cc3b90e27a96b2494fa4f5628
erbe1 <erik.bergstrom86@gmail.com>
2019-01-24 13:29:56 +0100

    no message

--- Tutor/Tutor/wwwroot/scripts/main.js
+++ Tutor/Tutor/wwwroot/scripts/main.js
@@ -7,28 +7,32 @@
 });
 
 
-function Link(linkadress, title, category, subCategory, embedded = false) {
+function Link(linkadress, title, category, subCategory, tag, description, embedded = false) {
     Linkadress = linkadress;
     Titel = title;
     Category = category;
     SubCategory = subCategory;
+    Tag = tag;
+    Description = description;
     Embedded = embedded;
 }
 
 let links = [
-    new {
-        Linkadress: "https://fontawesome.com/",
-        Title = "Font Awesome",
-        Category =["Frontend"],
-        SubCategory =["Fonts"],
-        Embedded = false
-    },
-
-];
-
-let links = [
     Link("https://fontawesome.com/", "Font Awesome", ["Frontend"], ["Fonts"]),
     Link("https://fontawesome.com/", "Font Awesome", ["Frontend"], ["Fonts"]),
+
+
+
+
+
+    Link("https://www.learncs.org/", "Learn C#", ["Backend"], ["C#"], ["C#", "Backend", "Interactive"], "Free interactive C# tutorial"),
+    Link("https://csharpskolan.se/", "C# skolan", ["Backend"], ["C#"], ["C#", "Svenska", "Backend"], "Utbildningsmaterial i form av instruktionsvideor och artiklar på svenska"),
+    Link("https://www.learnjavaonline.org/"), "Learn Java", ["Backend"], ["Java"], ["Java", "Backend", "Interactive"], "Free interactive Java tutorial"),
+    Link("https://www.codecademy.com/learn/learn-java"), "Codeacademy", ["Backend"], ["Java"], ["Java", "Backend", "Interactive"], "Fundamental programming concepts, including object-oriented programming (OOP) using Java"),
+    Link("https://www.techbeamers.com/python-tutorial-step-by-step/"), "TechBeamers", ["Backend"], ["Python"], ["Python", "Backend", "Free"], "This Python tutorial is a one-stop programming guide for all beginners"),
+
+
+
 ];
 
 
